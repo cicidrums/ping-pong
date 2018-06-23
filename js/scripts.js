d8a7bbc0bfc73eb23193cfed9914cb463e5fe923
Cici <cici.harrison@gmail.com>
2018-06-23 15:54:34 -0700

    feature an HTML file, a CSS folder with stylesheet, a JS folder with scripts.js, and a README.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//Business Logic
+var emptyArray = []
+
+var business = function(number) {
+  if (isNaN(number)) {
+    alert("Enter a number!");
+  } else 
+
+//UI Logic
+$(document).ready(function() {
+
+  $("form#ping").submit(function(event) {
+    event.preventDefault();
+
+    var number = $('input#number').val();
+
+    if ((number % 3 === 0))
+      alert("Yes!");
+    });
+  console.log(number);
+  });
+};
